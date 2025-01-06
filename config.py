19046a604eb9ae1770251161b3ca314901bbe376
patoalejor <patoalejor@gmail.com>
2025-01-06 22:58:50 +0900

    create the basic tree of the project

---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+import os
+
+# Configuration variables
+CALIBRATION_FOLDER = "calibration_data"
+OUTPUT_FOLDER = "output"
+CAMERA_IDS = [0,1] # Initial default
+CAMERA_COUNT = 2
+
+# Flask ports
+CALIBRATION_PORT = 5001
+POSE_2D_PORT = 5002
+POSE_3D_PORT = 5003
+MAIN_PORT = 5000
+
+# Create folders if they don't exist
+os.makedirs(CALIBRATION_FOLDER, exist_ok=True)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+# Calibration Parameters (Placeholder, will be updated after calibration)
+INTRINSIC_PARAMS = {}
+EXTRINSIC_PARAMS = {}
+CALIBRATED = False
